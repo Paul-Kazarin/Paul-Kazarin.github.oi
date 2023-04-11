d3311ee994da7078de51c8b2058ec7d1dfa90c90
Paul Kazarin <paul.kazarin@wgu.edu>
2023-04-11 15:06:46 -0600

    report start1

--- src/app/pages/report-page/report-page.component.ts
+++ src/app/pages/report-page/report-page.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {Router} from "@angular/router";
+import {FormControl, FormGroup} from "@angular/forms";
+import {MatDialog, MatDialogRef} from "@angular/material/dialog";
+import {InventoryService} from "../../services/inventory.service";
 
 @Component({
   selector: 'app-report-page',
@@ -8,11 +11,45 @@ import {Router} from "@angular/router";
 })
 export class ReportPageComponent implements OnInit {
 
+  reportPage!: FormGroup;
+
   constructor(
-    private router: Router
+    public dialogRef: MatDialogRef<ReportPageComponent>,
+    private router: Router,
+    private inventoryService: InventoryService,
+    public dialog: MatDialog
   ) { }
 
   ngOnInit(): void {
+    this.reportPage = new FormGroup({
+      type: new FormControl('', {}),
+      dateRange: new FormControl('', {})
+    })
+  }
+
+  clearForm(): void {
+    this.reportPage.reset();
+  }
+
+  onClose(): void {
+    this.dialogRef.close("closed");
+  }
+
+  onSubmit(): void {
+    if (this.reportPage.invalid) {
+      return;
+    } else {
+      const report = {
+        id: this.reportPage.value.id,
+        dateRange: this.reportPage.value.dateRange
+      };
+      console.log();
+      // this.inventoryService.postReportPageForm(report).subscribe(
+      //   result => console.log('success: ', result),
+      //   error => console.log('error: ', error),
+
+      this.onClose();
+    }
   }
 
   onHome(): void {
